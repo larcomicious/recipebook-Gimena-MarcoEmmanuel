aefd34f45bb8ff7fef5fb66e739fe0a71aad1912
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-03 23:45:08 +0800

    Created Ingredient, Recipe, and RecipeIngredient models. Made migrations for the models.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+class RecipeIngredient(models.Model):
+    quantity = models.FloatField(max_length=100)
+    ingredient = models.ForeignKey(
+        'Ingredient', 
+        on_delete = models.CASCADE,
+        related_name = 'ingredients'
+    )
+    recipe = models.ForeignKey(
+        'Recipe', 
+        on_delete = models.CASCADE,
+        related_name = 'recipe'
+    )
