c96ce00bb8ee6179007eab1d94d1a25b7d0d64ee
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-04 18:06:06 +0800

    Added string representation and get absolute url for the models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,23 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe-detail', args=[self.pk])
     
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe-list', args=[self.pk])
 
 class RecipeIngredient(models.Model):
-    quantity = models.FloatField(max_length=100)
+    quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(
         'Ingredient', 
         on_delete = models.CASCADE,
-        related_name = 'ingredients'
+        related_name = 'recipe'
     )
     recipe = models.ForeignKey(
         'Recipe', 
         on_delete = models.CASCADE,
-        related_name = 'recipe'
+        related_name = 'ingredients'
     )
